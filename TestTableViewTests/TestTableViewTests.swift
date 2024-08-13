9ab87bc787306e070bccbae5d9b9295e347f6616
KevinHung <kevinaugust145@gmail.com>
2024-08-13 14:33:39 +0800

    Initial Commit

---
+++ TestTableViewTests/TestTableViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestTableViewTests.swift
+//  TestTableViewTests
+//
+//  Created by 洪宗燦 on 2024/8/13.
+//
+
+import Testing
+@testable import TestTableView
+
+struct TestTableViewTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
